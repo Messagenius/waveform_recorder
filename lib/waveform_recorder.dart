9f82b84bd91e38f17b1d869563f2ff32c845fb17
LucazzP <lucashpolazzo2013@gmail.com>
2025-03-27 20:15:14 -0300

    Exported record package config classes

--- lib/waveform_recorder.dart
+++ lib/waveform_recorder.dart
@@ -1,2 +1,10 @@
+export 'package:record/record.dart'
+    show
+        AndroidAudioSource,
+        AndroidRecordConfig,
+        AudioEncoder,
+        IosAudioCategoryOption,
+        IosRecordConfig,
+        RecordConfig;
 export 'src/waveform_recorder.dart';
 export 'src/waveform_recorder_controller.dart';
