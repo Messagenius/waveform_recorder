94afb6cc77d7a3019a35d6cb4773cc9453b36683
LucazzP <lucashpolazzo2013@gmail.com>
2025-03-27 20:20:19 -0300

    Added missing audio config export

--- lib/waveform_recorder.dart
+++ lib/waveform_recorder.dart
@@ -3,8 +3,10 @@ export 'package:record/record.dart'
         AndroidAudioSource,
         AndroidRecordConfig,
         AudioEncoder,
+        InputDevice,
         IosAudioCategoryOption,
         IosRecordConfig,
         RecordConfig;
+
 export 'src/waveform_recorder.dart';
 export 'src/waveform_recorder_controller.dart';
